03c5b3bfbc4904030d1ea342a1a44f9fa536bc13
Blessed Mutengwa <blessedmutengwa@gmail.com>
2021-07-06 06:25:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -57,9 +57,14 @@ def get_test_data():
 
   x_test, y_test = [], []
 
-  for x in range(prediction_days, len(model_inputs)):
-    x_test.append(model_inputs[x-prediction_days:x, 0])
-    y_test.append(model_inputs[x, 0])
+  if days <= 1:
+    for x in range(prediction_days, len(model_inputs)):
+      x_test.append(model_inputs[x-prediction_days:x, 0])
+      y_test.append(model_inputs[x, 0])
+ else:
+  for x in range(prediction_days, len(model_inputs)-30):
+      x_test.append(model_inputs[x-prediction_days:x, 0])
+      y_test.append(model_inputs[x+30, 0])
     
   x_test, y_test = np.array(x_test), np.array(y_test)
   x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1],1))
